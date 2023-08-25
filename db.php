e244ebb2e4afa6c9ba250b7f71a8354fc1a394be
Riyaa24 <129434194+Riyaa24@users.noreply.github.com>
2023-08-26 00:54:03 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,13 @@
 <?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "users";
 
-$conn=mysqli_connect(
-    'localhost',
-    'root', '',
-    'rm_collection'
-);
-if($conn){
-    echo "Login Successfull";
-}else{
-    echo "Not Connected";
-}
-
+    $conn = mysqli_connect($server, $username, $password, $database);
+    if (!$conn){
+    //     echo "Success";
+    // }else{
+        die("Error" . mysqli_connect_error());
+    }
 ?>
